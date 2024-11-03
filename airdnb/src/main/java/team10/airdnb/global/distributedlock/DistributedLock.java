f65cedb0540ba9f25312215ebb6263ffad19b61a
choejieun <wldms5221@icloud.com>
2024-11-03 18:58:56 +0900

    feat: DistributedLock 어노테이션 추가 분산락을 적용할 메소드에 사용할 어노테이션 구현, 락에 사용할 키, 락의 시간단위, 대기시간, 점유시간 커스텀 가능

---
+++ airdnb/src/main/java/team10/airdnb/global/distributedlock/DistributedLock.java
@@ -0,0 +1,25 @@
+package team10.airdnb.global.distributedlock;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DistributedLock {
+
+    /* 락에 사용할 Key의 이름 : 필수값 */
+    String key();
+
+    /* 락의 시간 단위 : tryLock()의 TimeUnit 파라미터에 사용 */
+    TimeUnit timeUnit() default TimeUnit.SECONDS;
+
+    /* 락 획득을 위해 기다리는 시간 (default - 5s) : 락 획득을 위해 waitTime 만큼 대기 */
+    long waitTime() default 5L;
+
+    /* 락 점유 시간 (default - 3s) : 락을 획득한 이후 leaseTime 이 지나면 락을 해제한다 */
+    long leaseTime() default 3L;
+}
+
